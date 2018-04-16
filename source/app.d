855bd8606c5f247ecd61b2155412a407a1e4cdb9
Hiroki Noda <kubo39@gmail.com>
2018-04-17 04:47:04 +0900

    Initial implementation

--- source/app.d
+++ source/app.d
@@ -1,6 +1,99 @@
+/**
+Write a shell in C.  https://brennan.io/2015/01/16/write-a-shell-in-c/
+*/
+import std.file : chdir;
 import std.stdio;
+import std.string : cmp, split;
+
+
+int builtinCd(string[] args)
+{
+    if (args.length < 2)
+    {
+        stderr.writeln(`lsh: expected argument to "cd"`);
+    }
+    else
+    {
+        chdir(args[1]);
+    }
+    return 1;
+}
+
+class Builtins
+{
+    // Builtins should A-Z order.
+    int function(string[])[string] builtinMap;
+
+    this()
+    {
+        this.builtinMap = [
+            "cd": &builtinCd
+            ];
+    }
+
+    string[] keys()
+    {
+        return this.builtinMap.keys();
+    }
+
+    int function(string[]) get(string builtin)
+    {
+        return this.builtinMap[builtin];
+    }
+}
+
+class Lsh
+{
+    Builtins builtins;
+
+    this()
+    {
+        this.builtins = new Builtins();
+    }
+
+    void loop()
+    {
+        int status;
+        do
+        {
+            write("> ");
+            auto line = readln();
+            auto args = line.split();
+            status = this.execute(args);
+        }
+        while (status);
+    }
+
+    void launch(string[] args)
+    {
+        import std.process : spawnProcess, wait;
+        auto pid = spawnProcess(args);
+        scope(exit) wait(pid);
+    }
+
+    int execute(string[] args)
+    {
+        if (args.length < 1)
+        {
+            return 1;
+        }
+
+        foreach (builtin; this.builtins.keys())
+        {
+            if (cmp(args[0], builtin) == 0)
+            {
+                return (*this.builtins.get(builtin))(args);
+            }
+        }
+        this.launch(args);
+        return 1;
+    }
+
+
+}
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+    auto lsh = new Lsh;
+    lsh.loop();
 }
